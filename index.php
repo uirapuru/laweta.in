9337b545609c1dcd80cc3dd6924a22a3560f5094
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2015-11-23 00:14:22 +0100

    up

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
+        <title>Laweta.in - Transport krajowy i międzynarodowy! Bus/Laweta! Wrocław - Wrocław, Dolnośląskie, Krzyki</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -13,14 +13,98 @@
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+        <style>
+            body {
+                padding-top: 50px;
+            }
+
+            div.container {
+                width: 990px;
+            }
+
+            .starter-template {
+                padding: 40px 15px;
+                text-align: center;
+            }
+
+        </style>
+
     </head>
     <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+    <!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <div class="container">
+            <div class="starter-template">
+                <div class="lead">
+                    <p>Zapraszamy do współpracy oraz do zapoznania się z naszą ofertą, telefon: 793 698 171</p>
+                    <p>Transport z nami to bezpieczeństwo i wygoda !</p>
+                    Oferujemy Państwu expresowy transport na trasach krajowych i międzynarodowych.<br/>
+                    Specjalizujemy się w przewozie samochodów, motocykli,maszyn oraz wszelkiego rodzaju towarów.<br/>
+                    Usługi transportowe świadczymy na terenie całego kraju oraz krajów europejskich.<br/>
+                </div>
+                <p>
+                    W naszej ofercie posiadamy:
+                    <ul>
+                        <li>BUSY TOWAROWE,</li>
+                        <li>AUTO/LAWETY</li>
+                        <li>LAWETY</li>
+                    </ul>
+                </p>
+
+                <div class="row">
+                    <div class="col-xs-6 col-md-6">
+                        <a href="#" class="thumbnail">
+                            <img src="img/001.jpg" alt="...">
+                        </a>
+                    </div>
+                    <div class="col-xs-6 col-md-6">
+                        <a href="#" class="thumbnail">
+                            <img src="img/002.jpg" alt="...">
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-6 col-md-6">
+                        <a href="#" class="thumbnail">
+                            <img src="img/003.jpg" alt="...">
+                        </a>
+                    </div>
+                    <div class="col-xs-6 col-md-6">
+                        <a href="#" class="thumbnail">
+                            <img src="img/004.jpg" alt="...">
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-6 col-md-6">
+                        <a href="#" class="thumbnail">
+                            <img src="img/005.jpg" alt="...">
+                        </a>
+                    </div>
+                    <div class="col-xs-6 col-md-6">
+                        <a href="#" class="thumbnail">
+                            <img src="img/006.jpg" alt="...">
+                        </a>
+                    </div>
+                </div>
+
+                <p>Zapraszamy do współpracy oraz do zapoznania się z naszą ofertą, telefon: 793 698 171</p>
+                <p>Transport z nami to bezpieczeństwo i wygoda !</p>
+            </div>
+
+        </div><!-- /.container -->
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
